b3fa39a03fc994c2339ee7382a651239d6a688dc
khakimovSamdu <xakimov2806@gmail.com>
2026-02-16 13:33:05 +0500

    update footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
         <div class="footer-content">
-            <p>&copy; 2025 SamCoding. Created by Khakimov Allamurod</p>
+            <p>&copy; 2026 SamCoding. Created by Students of the Faculty of Artificial Intelligence and Digital Technologies, Samarkand State University</p>
             <div class="footer-links">
                 <a href="https://www.codewars.com/users/allamurodxakimov">Codewars</a>
                 <a href="https://www.instagram.com/allamurod_xakimov/">Instagram</a>
